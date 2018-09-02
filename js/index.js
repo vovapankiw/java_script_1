6b438a316e7f3d1abce3319213b8d0b5c6e4d4f6
Vova Pankiv <vovapankiw@gmail.com>
2018-09-02 10:46:43 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
-function checkAge(age) {
-    if(age >= 18 && age < 100){
-        return 'adult';
-    } else if(age > 100 ) {
-        return "You are old";
-    }else{
-        return 'child';
+alert('Please, enter a number')
+const a = prompt('Enter variable a');
+const b = prompt('Enter variable b');
+const c = prompt('Enter variable c');
+
+function descriminant(a, b, c) {
+    if (isNaN(a) || isNaN(b) || isNaN(c) || a=='' || b =='' || c ==''){
+        alert('You entered not a number');
+        return -1;
+    }
+    return  b * b - 4 * a * c;
+}
+const D = descriminant(a, b, c)
+
+function solvEquasion (){
+    if (D > 0){
+        const x = (-b + Math.sqrt(D)) / (2 * a);
+        const y = (-b - Math.sqrt(D)) / (2 * a);
+        return `First solution = ${x.toFixed(2)}, \nSecond solution = ${y.toFixed(2)}`;
+    }  else if (D === 0){
+        const x = (-b + Math.sqrt(D)) / (2 * a);
+        return `Only one possible solution = ${x.toFixed(2)}`;
+    } else {
+        return( 'This equasion doesn\'t have possible solution')
     }
 }
-
-const userAge = parseInt(prompt('Enter your age'));
-const message = checkAge(userAge);
-alert(message);
+alert(solvEquasion());
